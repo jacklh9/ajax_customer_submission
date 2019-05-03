3aa3e7114e48a9985a43b564b7297ef88a78a705
Jack L. Hamilton <jacklh@gmail.com>
2019-05-03 04:02:19 -0700

    Change path to vendor/autoload.php from relative to absolute

--- includes/s3.php
+++ includes/s3.php
@@ -2,7 +2,7 @@
 
     $bucket = getenv('S3_BUCKET', true) ?: getenv('S3_BUCKET');
     if(!empty($bucket)){
-        require('vendor/autoload.php');
+        require('/app/vendor/autoload.php');
         // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
         $s3 = new Aws\S3\S3Client([
             'version'  => '2006-03-01',
