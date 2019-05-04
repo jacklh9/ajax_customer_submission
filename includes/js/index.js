4c8c48820c2e5d1df77638109bc8dab92ba81abb
Jack L. Hamilton <jacklh@gmail.com>
2019-05-04 16:31:21 -0700

    Move index.php js to includes/js/index.js

---
+++ includes/js/index.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+    <?php include "includes/js/functions.js"; ?>
+
+    /* ************* LOGIN FORM ******************* */
+
+    $('#form-container').hide();
+    $('#notification-bar').hide();
+    $('#login-user-form').show();
+    $('#upload-user-form').hide();
+    $('#form-container').show();
+    show_registered_users();
+
+    $('#login-user-form').submit(function(evt){
+        evt.preventDefault();
+    
+        var url = $(this).attr('action');
+        var data = $(this).serialize();
+
+        $.post(url, data, function(response){
+            $('#submission-form-row').html(response);
+            $('#login-user-form').hide();
+            $('#notification-bar').hide();
+            $('#registered-users-list').hide();
+        });
+
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
