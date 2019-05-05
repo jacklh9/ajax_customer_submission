1bdbef78c6b0a47fd051a9dea917ffe141fd9727
Jack L. Hamilton <jacklh@gmail.com>
2019-05-05 06:02:06 -0700

    Move preview pic JS to external file

--- includes/submission_form.php
+++ includes/submission_form.php
@@ -1,18 +1,6 @@
 <?php include_once "includes/functions.php"; ?>
 <?php include_once "s3.php"; ?>
-<script>
-    // ADD PREVIEW PIC
-    function addPreviewPic(obj){
-        
-        // When this input is changed, the below native-JS will update the profile pic preview image
-        document.getElementById('profile-pic').src = window.URL.createObjectURL(obj.files[0]);
-
-        // We then remove the delete button because the delete button will never be able to clear the input control
-        // and will only delete the preview image, but on submission the input field will put the picture back.
-        // It causes confusion and is best to just hide the delete button at this juncture. 
-        $('#btn-delete-profile-pic').hide();
-    }
-</script>
+<?php include_once "./includes/js/dynamic_preview_pic.js"; ?>
 <form action="upload.php" id="upload-user-form" method="post" enctype="multipart/form-data">
 
     <div class="row" id="personal-info-row">
